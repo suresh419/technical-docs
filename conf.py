22b04d208ef6dc164c3f7a25852a3e9430352902
suresh419 <79440827+suresh419@users.noreply.github.com>
2021-02-23 16:44:31 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'ABC'
-copyright = '2021, Harideep'
-author = 'Harideep'
+project = 'git & github'
+copyright = '2021, suresh'
+author = 'suresh'
 
 # The full version, including alpha/beta/rc tags
 release = '2021'
